629c6108a6ccc7a2a4ccc00771b9b46d614a0e58
Dai Nguyen <ncdai3651408@gmail.com>
2021-10-11 11:22:46 +0700

    Improve validate form and add README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simple Calculator
+
+Bài tập Bootstrap, JavaScript - Môn Phát triển ứng dụng Web
+
+## Thông tin sinh viên
+
+- Họ tên : Nguyễn Chánh Đại
+- MSSV : 18120113
+- Email : 18120113@student.hcmus.edu.vn
+
+## Bảng điểm
+
+| Nội dung | <div style="width: 64px">Điểm</div> | <div style="width: 164px">Tự đánh giá</div> |
+|-|:-:|:-:|
+| Sử Bootstrap để làm giao diện, canh thẳng theo cột, chỉ có thể chọn một trong bốn phép tính | 2 | 2 |
+| Viết mã Javascript để khi bấm vào nút tính, kết quả của phép tính đang chọn sẽ được điền vào ô kết quả | 4 | 4 |
+| Vùng thông báo sẽ hiển thị thông báo lỗi khi :<br /><ul><li>Di chuyển khỏi ô nhập Số thứ nhất hoặc Số thứ hai mà dữ liệu không phải là số thực</li><li>Bấm nút Tính mà chưa chọn phép tính</li><li>Chưa điền đủ hai số hợp lệ để thực hiện phép tính</li></ul> | 4 | 4 |
+| **[Điểm cộng]** Giao diện hiển thị thông báo lỗi thân thiện với người dùng | - | - |
+| **[Điểm cộng]** Giao diện responsive, thẩm mỹ, thân thiện và dễ sử dụng | - | - |
+| **[Điểm cộng]** Đăng tải lên host thực tế<br />https://simple-calculator.ncdai.name.vn | - | - |
--- assets/js/app.js
+++ assets/js/app.js
@@ -3,63 +3,125 @@ var formEl = document.getElementById("form");
 var inputNumber1El = document.getElementById("inputNumber1");
 var inputNumber2El = document.getElementById("inputNumber2");
 var inputResultEl = document.getElementById("inputResult");
+var inputOperatorSelector = 'input[name="operator"]';
 
 var number1InvalidFeedback = document.getElementById("number1InvalidFeedback");
 var number2InvalidFeedback = document.getElementById("number2InvalidFeedback");
+var operatorInvalidFeedback = document.getElementById("operatorInvalidFeedback");
 
 function cal(a, b, operator) {
   switch (operator) {
     case "sum":
       return a + b;
+
     case "sub":
       return a - b;
+
     case "mul":
       return a * b;
+
     case "div":
-      if (b === 0) {
+      if (b == 0) {
+        inputResultEl.value = "";
+        displayInputError(inputNumber2El, number2InvalidFeedback, "Không thể thực hiện phép chia cho 0. Vui lòng nhập số thứ 2 khác 0!");
         throw new Error("Divide by zero");
       }
+
+      inputNumber2El.classList.remove("is-invalid");
+
       return a / b;
+
     default:
       return 0;
   }
 }
 
-function getOperator() {
-  return document.querySelector('input[name="operator"]:checked').value;
+function displayInputError(inputEl, invalidFeedbackEl, errorMessage) {
+  invalidFeedbackEl.textContent = errorMessage;
+  inputEl.classList.add("is-invalid");
 }
 
-function validateForm() {
+function validateInputNumber1() {
+  if (!inputNumber1El.value) {
+    displayInputError(inputNumber1El, number1InvalidFeedback, "Vui lòng nhập một số thứ 1!");
+    return false;
+  }
+
   var number1 = Number(inputNumber1El.value);
-  var number2 = Number(inputNumber2El.value);
-  var operator = getOperator();
 
   if (Number.isNaN(number1)) {
-    number1InvalidFeedback.textContent = "Giá trị số thứ 1 không phải là số. Vui lòng nhập một số hợp lệ!";
-    inputNumber1El.classList.add("is-invalid");
+    displayInputError(inputNumber1El, number1InvalidFeedback, "Giá trị số thứ 1 không phải là số. Vui lòng nhập một số hợp lệ!");
     return false;
-  } else {
-    inputNumber1El.classList.remove("is-invalid");
   }
+
+  inputNumber1El.classList.remove("is-invalid");
+
+  return true
+}
+
+function validateInputNumber2() {
+  if (!inputNumber2El.value) {
+    displayInputError(inputNumber2El, number2InvalidFeedback, "Vui lòng nhập một số thứ 2!");
+    return false;
+  }
+
+  var number2 = Number(inputNumber2El.value);
 
   if (Number.isNaN(number2)) {
-    number2InvalidFeedback.textContent = "Giá trị số thứ 2 không phải là số. Vui lòng nhập một số hợp lệ!";
-    inputNumber2El.classList.add("is-invalid");
+    displayInputError(inputNumber2El, number2InvalidFeedback, "Giá trị số thứ 2 không phải là số. Vui lòng nhập một số hợp lệ!");
     return false;
-  } else {
-    inputNumber2El.classList.remove("is-invalid");
   }
 
-  if (operator == "div" && number2 == 0) {
-    number2InvalidFeedback.textContent = "Không thể thực hiện phép chia cho 0. Vui lòng nhập số thứ 2 khác 0!";
-    inputNumber2El.classList.add("is-invalid");
+  inputNumber2El.classList.remove("is-invalid");
+
+  return true
+}
+
+function validateInputOperator() {
+  var items = document.querySelectorAll(inputOperatorSelector);
+
+  if (!document.querySelector(inputOperatorSelector + ":checked")) {
+    items.forEach(function(item) {
+      item.classList.add("is-invalid");
+    });
+    operatorInvalidFeedback.textContent = "Vui lòng chọn phép tính!";
+    operatorInvalidFeedback.classList.add("d-block");
     return false;
-  } else {
-    inputNumber2El.classList.remove("is-invalid");
   }
+
+  items.forEach(function(item) {
+    item.classList.remove("is-invalid");
+  });
+  operatorInvalidFeedback.classList.remove("d-block");
 
   return true;
 }
+
+function validateForm() {
+  var check = true;
+
+  if (!validateInputNumber1()) {
+    check = false;
+  }
+
+  if (!validateInputNumber2()) {
+    check = false;
+  }
+
+  if (!validateInputOperator()) {
+    check = false;
+  }
+
+  return check;
+}
+
+inputNumber1El.addEventListener("blur", function() {
+  validateInputNumber1();
+});
+
+inputNumber2El.addEventListener("blur", function() {
+  validateInputNumber2();
+});
 
 formEl.addEventListener("submit", function(e) {
   e.preventDefault();
@@ -71,7 +133,7 @@ formEl.addEventListener("submit", function(e) {
 
     var number1 = Number(inputNumber1El.value);
     var number2 = Number(inputNumber2El.value);
-    var operator = getOperator();
+    var operator = document.querySelector(inputOperatorSelector + ":checked").value;
 
     var result = cal(number1, number2, operator);
     inputResultEl.value = result;
